e0b74a8c4f29c6c7e2828ed3d7773add81fdbd11
1aayuss <ayush.makwana700@gmail.com>
2024-02-16 00:49:16 +0530

    Fixed account's background

--- app/(dashboard)/(routes)/account/page.tsx
+++ app/(dashboard)/(routes)/account/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const AccountPage = () => {
     return (
 
-        <div className='flex items-center justify-center mb-14'>
+        <div className='flex items-center justify-center mb-14 bg-slate-100'>
             <UserProfile />
         </div>
     )
